11cb88b0f25040fd36b609668c50e1938af9acc4
samisdat <bastian@pertz.eu>
2020-09-23 22:51:06 +0200

    formt code from pdf

--- data-source.js
+++ data-source.js
@@ -1,9 +1,33 @@
-const { interval, merge } = rxjs; const { map } = rxjs.operators;
-const createTweetSource = (frequency, account, attribute) => { return interval(frequency).pipe(map(i => ({
-    account,
-    timestamp: Date.now(),
-    content: `${attribute} Tweet number ${i + 1}` })));
+const { interval, merge } = rxjs;
+const { map } = rxjs.operators;
+
+const createTweetSource = (frequency, account, attribute) => {
+    return interval(frequency).pipe(
+        map(i => ({
+            account,
+            timestamp: Date.now(),
+            content: `${attribute} Tweet number ${i + 1}` })
+        )
+    );
 }
 const tweets = merge(
-    createTweetSource(5000, 'AwardsDarwin', 'Facepalm'), createTweetSource(3000, 'iamdevloper', 'Expert'), createTweetSource(5000, 'CommitStrip', 'Funny')
-); tweets.subscribe(console.log.bind(console));
+    createTweetSource(
+        5000,
+        'AwardsDarwin',
+        'Facepalm'
+    ),
+    createTweetSource(
+        3000,
+        'iamdevloper',
+        'Expert'
+    ),
+    createTweetSource(
+        5000,
+        'CommitStrip',
+        'Funny'
+    )
+);
+
+tweets.subscribe(
+    console.log.bind(console)
+);
