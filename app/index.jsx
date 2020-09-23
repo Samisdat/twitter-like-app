4ad403116bbd054b023cb9cd221de89ad357098b
samisdat <bastian@pertz.eu>
2020-09-23 23:29:59 +0200

    start with react and finish this day

--- app/index.jsx
+++ app/index.jsx
@@ -4,3 +4,19 @@ import {tweets} from "./data/mock-tweets";
 tweets.subscribe(
     console.log.bind(console)
 );*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+function TwitterLikeApp() {
+
+    return (
+        <React.Fragment>
+            <h1>Hello from react</h1>
+        </React.Fragment>
+    );
+}
+
+let appHtmlElement = document.getElementById('twitter-like-app');
+
+ReactDOM.render(<TwitterLikeApp />, appHtmlElement);
