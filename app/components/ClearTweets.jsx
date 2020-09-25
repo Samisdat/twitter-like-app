d774588ebdd7f41400475d4cce1d088c5a498479
samisdat <bastian@pertz.eu>
2020-09-26 00:25:59 +0200

    uese arrow fn and one whitespace

--- app/components/ClearTweets.jsx
+++ app/components/ClearTweets.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import {Button} from 'react-bootstrap'
 
-export function ClearTweets(props) {
-    
+export const ClearTweets = (props) => {
+
     const disabled = (0 === props.numberOfTweets) ? 'disabled' : '';
 
     return (
-        <Button disabled={disabled} variant="danger" onClick={props.clearTweetsInStore} >Clear tweets</Button>
+        <Button disabled={disabled} variant="danger" onClick={props.clearTweetsInStore}>Clear tweets</Button>
     );
 }
